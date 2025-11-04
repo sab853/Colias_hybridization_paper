f1fda365ed057ee4a9b8e3ca6da46dceebd72d03
Amanda <158127209+sab853@users.noreply.github.com>
2025-11-04 13:56:26 -0500

    Add files via upload
    Script to create Butterfly map from points of collection

---
+++ Butterfly_map.R
@@ -0,0 +1,24 @@
+#Script to create Butterfly maps from points of collection
+
+#load packages
+library(ggplot2)
+library(ggmap)
+library(readxl)
+
+api_secret <-"" #insert unique API code here
+register_google(key = api_secret)
+
+#Read excel
+Fieldwork_datasheet <- read_excel("Fieldwork datasheet .xlsx")
+View(Fieldwork_datasheet)
+
+#Butterfly map
+mapbutterfly <- get_map (location=c(Longitude=(-79.38332), Latitude=(43.70885)), zoom =11, scale=1)
+ggmap(mapbutterfly)
+
+mapbutterfly1<- ggmap(mapbutterfly)+ geom_point(data=Fieldwork_datasheet,aes(x=Longitude,y=Latitude, alpha = 0.7),
+                                                size = 4, shape = 21, colour="blue", fill="blue") +guides(fill=FALSE, alpha=FALSE, size=FALSE) +labs(y= "Latitude", x = "Longitude")
+mapbutterfly1
+
+
+
