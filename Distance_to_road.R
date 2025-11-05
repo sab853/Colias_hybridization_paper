a33c128f75603ce98ac06c8a88c6fb1f466648e7
Amanda <158127209+sab853@users.noreply.github.com>
2025-11-05 14:29:01 -0500

    Update Distance_to_road.R

--- Distance_to_road.R
+++ Distance_to_road.R
@@ -8,7 +8,7 @@ library(dplyr)
 library(geosphere)
 library(readxl)
 
-Fieldwork_datasheet <- read_excel("Fieldwork datasheet .xlsx")
+Fieldwork_datasheet <- read_excel("Fieldwork_datasheet_2024.xlsx")
 View(Fieldwork_datasheet)
 
 name1<- Fieldwork_datasheet$`Park Name`
